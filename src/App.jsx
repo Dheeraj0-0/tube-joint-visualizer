736666ce061646886a6e19d60656838613367892
Dheeraj Parihar <117371624+Dheeraj0-0@users.noreply.github.com>
2025-11-19 17:23:23 +0530

    Create App component with TubeJointVisualizer

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TubeJointVisualizer from './TubeJointVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <TubeJointVisualizer />
+    </div>
+  );
+}
+
+export default App;
